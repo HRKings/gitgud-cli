76ced698a2fa7c5c9158e2c167d3c29f5022df5e
Helton Reis <heltonkreis@gmail.com>
2021-12-21 19:18:19 -0300

    [docs](Flow) Add comments to code

--- flow/flow.go
+++ flow/flow.go
@@ -28,6 +28,7 @@ var Command = cli.Command{
 				remote := context.String("remote")
 				defaultBranch := context.String("defaultBranch")
 
+				// If we don't have a local repository, create one
 				err := utils.HasRepository()
 				if err != nil {
 					stdout, stderr, gitErr := utils.ExecGit("init")
@@ -39,6 +40,7 @@ var Command = cli.Command{
 					color.Blue("> Created a new Git repository...")
 				}
 
+				// Create the first commit
 				stdout, stderr, gitErr := utils.ExecGit("commit",
 					"--allow-empty", "-m", "[misc] Initial commit")
 				err = utils.HandleGitError(stdout, stderr, gitErr)
@@ -47,6 +49,7 @@ var Command = cli.Command{
 				}
 				color.Blue("> Created the initial commit...")
 
+				// Crete the stable branch
 				stdout, stderr, gitErr = utils.ExecGit("checkout", "-b", "stable")
 				err = utils.HandleGitError(stdout, stderr, gitErr)
 				if err != nil {
@@ -54,11 +57,9 @@ var Command = cli.Command{
 				}
 				color.Blue("> Created branch stable...")
 
+				// If we have a remote
 				if remote != "" {
-
-				}
-
-				if remote != "" {
+					// Add the remote to the local repo
 					stdout, stderr, gitErr = utils.ExecGit("remote", "add", "origin", remote)
 					err = utils.HandleGitError(stdout, stderr, gitErr)
 					if err != nil {
@@ -66,6 +67,7 @@ var Command = cli.Command{
 					}
 					color.Blue("> Added remote 'origin' pointing to '%s'", remote)
 
+					// Push the default branch to the remote
 					stdout, stderr, gitErr = utils.ExecGit("push", "-u", "origin", defaultBranch)
 					err = utils.HandleGitError(stdout, stderr, gitErr)
 					if err != nil {
@@ -73,6 +75,7 @@ var Command = cli.Command{
 					}
 					color.Blue("> Pushed '%s' to 'origin'", defaultBranch)
 
+					// Push the stable branch to the remote
 					stdout, stderr, gitErr = utils.ExecGit("push", "-u", "origin", "stable")
 					err = utils.HandleGitError(stdout, stderr, gitErr)
 					if err != nil {
@@ -81,6 +84,7 @@ var Command = cli.Command{
 					color.Blue("> Pushed 'stable' to 'origin'")
 				}
 
+				// Go back to the default branch
 				stdout, stderr, gitErr = utils.ExecGit("checkout", defaultBranch)
 				err = utils.HandleGitError(stdout, stderr, gitErr)
 				if err != nil {
@@ -88,7 +92,7 @@ var Command = cli.Command{
 				}
 				color.Blue("> Changed to default branch '%s'", defaultBranch)
 
-				// Print the stdout of git
+				// Print the success message
 				color.Green("Fully initialized the repository")
 				return nil
 			},
