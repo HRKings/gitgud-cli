9686b87777be3e70b61e50abb748c5a6fee1cb3f
Helton Reis <hrkings@rhaizzing.com>
2021-12-23 01:47:02 -0300

    [chore] Change CLI engine to Cobra

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"github.com/HRKings/gitgud-cli/commit"
-	"github.com/HRKings/gitgud-cli/flow"
-	"github.com/urfave/cli/v2"
-	"log"
-	"os"
-)
+import "github.com/HRKings/gitgud-cli/cmd"
 
 func main() {
-	app := &cli.App{
-		Name: "gitgud",
-		Usage: "A cross-platform command line interface for the GitGud modular Git model",
-		Commands: []*cli.Command{
-			&commit.Command,
-			&flow.Command,
-		},
-	}
-	app.UseShortOptionHandling = true
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
+	cmd.Execute()
 }
--- utils/gitexec.go
+++ utils/gitexec.go
@@ -23,6 +23,22 @@ func ExecGit(subcommand string, arguments ...string) (string, string, error) {
 	return string(standardOutput.Bytes()), string(standardError.Bytes()), err
 }
 
+func HandleGitError(stdout string, stderr string, err error) error {
+	// Print the stdout and stderr in case of an error
+	if err != nil && stderr != "" {
+		color.Red(stderr)
+		return err
+	} else if err != nil && stdout != "" {
+		color.Red(stdout)
+		return err
+	} else if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
 func CanCommit() error {
 	stdout, stderr, err := ExecGit("status")
 	if err != nil {
@@ -40,25 +56,25 @@ func CanCommit() error {
 	return nil
 }
 
+func DoCommit(message string, stageTracked bool, amend bool) (string, string, error) {
+	var flags []string
+
+	if stageTracked {
+		flags = append(flags, "-a")
+	}
+
+	if amend {
+		flags = append(flags, "--amend")
+	}
+
+	flags = append(flags, "-m", message)
+	return ExecGit("commit", flags...)
+}
+
 func HasRepository() error {
 	_, stderr, err := ExecGit("rev-parse", "--is-inside-work-tree")
 	if err != nil {
 		return errors.New(stderr)
-	}
-
-	return nil
-}
-
-func HandleGitError(stdout string, stderr string, err error) error {
-	// Print the stdout and stderr in case of an error
-	if err != nil && stderr != "" {
-		color.Red(stderr)
-		return err
-	} else if err != nil && stdout != "" {
-		color.Red(stdout)
-		return err
-	} else if err != nil {
-		return err
 	}
 
 	return nil
